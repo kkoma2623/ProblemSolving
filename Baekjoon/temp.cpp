48f605edcc5fc330e97e0bdb2c59f7963c20a4d8
kkoma2623 <boo106@kookmin.ac.kr>
2021-10-03 14:35:59 +0900

    Baekjoon 21611 simulation GG

--- Baekjoon/temp.cpp
+++ Baekjoon/temp.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-vector<string> vec;
+unsigned int PRNG() {
+  static unsigned int seed = 5323;
+  seed = 8253729 * seed + 2396403;
+  
+  return seed % 3 + 1;
+}
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  
-  for(int i=0; i<100000; ++i){
-    cout << " 2 2";
+  int inputNum; cin >> inputNum;
+  for(int i=0; i<inputNum; ++i){
+    for(int j=0; j<inputNum; ++j){
+      cout << PRNG() << " ";
+    }
+    cout << "\n";
   }
 
   return 0;
