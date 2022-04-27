15a65f5a7d0ff7a9522e49a2958e50ce429d204a
kkoma2623 <jiwook.dev@gmail.com>
2022-04-27 14:38:59 +0900

    Baekjoon 21611 GG

--- Baekjoon/21611Baek.cpp
+++ Baekjoon/21611Baek.cpp
@@ -1,305 +1,271 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <queue>
-#include <stack>
 
-#define MAX_N 49
+#define MAX_N 55
 
 using namespace std;
 
-typedef long long ll;
-
 int N, M;
-ll G[MAX_N+1][MAX_N+1];
+int G[MAX_N+1][MAX_N+1];
 int dir[5][2] = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-int glassDir[4][2] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-ll aEx, bEx, cEx;
-
-struct spell
-{
-    int d, s;
+int goDir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+struct pos{
+    int r, c;
+    int s;
 };
-struct AB
-{
-    int A, B;
+struct group{
+    int g;
+    int cnt;
 };
-queue<spell> q;
+vector<pos> ds;
+vector<int> changeDirPos;
+map<int, pos> m;
+int g1, g2, g3;
 
-void getInput()
-{
+void getInputs(){
     cin >> N >> M;
-    for(int r=1; r<=N; ++r)
-    {
-        for(int c=1; c<=N; ++c)
-        {
-            int val; cin >> val;
-            G[r][c] = val;
+    for(int r=1; r<=N; ++r){
+        for(int c=1; c<=N; ++c){
+            cin >> G[r][c];
         }
     }
-    for(int i=0; i<M; ++i)
-    {
-        int di, si; cin >> di >> si;
-        q.push({di, si});
+    for(int i=0; i<M; ++i){
+        int d, s; cin >> d >> s;
+        ds.push_back({dir[d][0], dir[d][1], s});
     }
 }
 
-void countExplodedGlass(int val)
-{
-    switch(val)
-    {
-        case 1:
-            ++aEx;
-            break;
-        case 2:
-            ++bEx;
-            break;
-        case 3:
-            ++cEx;
-            break;
-        default:
-            break;
+void setChangeDirPos(){
+    vector<int> temp;
+    for(int i=1; i<N; ++i){
+        temp.push_back(i);
+        temp.push_back(i);
+    }
+
+    int ret = 0;
+    changeDirPos.push_back(ret);
+    for(auto t: temp){
+        ret += t;
+        changeDirPos.push_back(ret);
     }
 }
 
-void shootBlizard()
-{
-    int di = q.front().d, si = q.front().s;
-    q.pop();
+bool outOfBound(int r, int c){
+    return (r<1 || c<1 || r>N || c>N);
+}
 
-    for(int i=1; i<=si; ++i)
-    {
-        int nr = N/2 + 1 + dir[di][0] * i, nc = N/2 + 1 + dir[di][1] * i;
-        if(nr < 1 || nc < 1 || nr > N || nc > N)
-        {
-            break;
+void blizard(int magicNum){
+    int r = ds[magicNum].r, c = ds[magicNum].c, s = ds[magicNum].s;
+    int sharkR = (N+1)/2, sharkC = (N+1)/2;
+    int currR = r + sharkR, currC = c + sharkC;
+    for(int i=1; i<=s; ++i){
+        if(outOfBound(currR, currC)) return;
+        G[currR][currC] = 0;
+        currR += r;
+        currC += c;
+    }
+}
+
+void setGIndex(){
+    int posNum = 0;
+    int posDiff = 0;
+    int goDirPos = 0;
+
+    int r = (N+1)/2, c = (N+1)/2;
+    pos curr = {r, c};
+    for(int i=0; i<N*N; ++i){
+        int currPos = i;
+        m[i] = curr;
+        if(changeDirPos[posNum] == currPos){
+            ++goDirPos;
+            ++posNum;
         }
-        G[nr][nc] = 0;
+        curr = {curr.r + goDir[goDirPos%4][0], curr.c + goDir[goDirPos%4][1]};
     }
 }
 
-void changeGlassDir(int &currGlassDir, int &nr, int &nc)
-{
-    if(nr-1 == nc && nc < N/2 + 1)
-    {
-        currGlassDir = 1;
-    }
-    else if(nr == nc && nr > N/2 + 1)
-    {
-        currGlassDir = 3;
-    }
-    else if(nr + nc == N+1 && nr > nc)
-    {
-        currGlassDir = 2;
-    }
-    else if(nr + nc == N+1 && nr < nc)
-    {
-        currGlassDir = 0;
-    }
-}
-
-void clearG()
-{
-    for(int r=1; r<=N; ++r)
-    {
-        for(int c=1; c<=N; ++c)
-        {
-            G[r][c] = 0;
-        }
-    }
-}
-
-void initG(queue<int> &glassQ)
-{
-    clearG();
-    int currGlassDir = 0;
-    int currR=N/2 + 1, currC=N/2 + 1;
-
-    while(!glassQ.empty())
-    {
-        int val = glassQ.front();
-        glassQ.pop();
-        currR = currR + glassDir[currGlassDir][0];
-        currC = currC + glassDir[currGlassDir][1];
-        G[currR][currC] = val;
-        if(currR == 1 && currC == 1)
-            break;
-        if(currR > N || currC > N || currR < 1 || currC < 1)
-            continue;
-        changeGlassDir(currGlassDir, currR, currC);
-    }
-}
-
-queue<int> queueGlass()
-{
-    queue<int> glassQ;
-    int currR=N/2 + 1, currC=N/2 + 1;
-    int currGlassDir = 0;
-    while(currR != 1 || currC != 1)
-    {
-        changeGlassDir(currGlassDir, currR, currC);
-        currR = currR + glassDir[currGlassDir][0], currC = currC + glassDir[currGlassDir][1];
-
-        if(G[currR][currC] == 0)
-        {
-            continue;
-        }
-        glassQ.push(G[currR][currC]);
-    }
-
-    return glassQ;
-}
-
-void moveGlass()
-{
-    queue<int> glassQ = queueGlass();
-    initG(glassQ);
-}
-
-void tempQ2SavedQ(queue<int> &tempQ, queue<int> &savedQ)
-{
-    while(!tempQ.empty())
-    {
-        int t = tempQ.front(); tempQ.pop();
-        savedQ.push(t);
-    }
-}
-
-void clearTempQ(queue<int> &tempQ)
-{
-    while(!tempQ.empty())
-    {
-        int glassVal = tempQ.front(); tempQ.pop();
-        countExplodedGlass(glassVal);
-    }   
-}
-
-void explodeGlass()
-{
-    bool chainExplode = true;
-    while(chainExplode)
-    {
-        queue<int> glassQ = queueGlass();
-        int cnt = 0;
-        queue<int> savedQ;
-        queue<int> tempQ;
-        chainExplode = false;
-        while(!glassQ.empty())
-        {
-            int currGlass = glassQ.front(); glassQ.pop();
-            int nextGlass = glassQ.front();
-            tempQ.push(currGlass);
-            if(currGlass != nextGlass && cnt < 3)
-            {
-                cnt = 0;
-                tempQ2SavedQ(tempQ, savedQ);
-            }
-            else if(currGlass != nextGlass && cnt >= 3)
-            {
-                cnt = 0;
-                chainExplode = true;
-                clearTempQ(tempQ);
-                continue;
-            }
-
-            if(currGlass == nextGlass)
-                ++cnt;
-            if(cnt >= 3)
-            {
-                clearTempQ(tempQ);
-                chainExplode = true;
-                continue;
-            }            
-        }
-        initG(savedQ);
-    }
-}
-
-void resetG()
-{
-    stack<AB> abS;
-    int currR = N/2 + 1, currC = N/2;
-    int currGlassDir = 0;
-    int currVal = G[currR][currC];
-    abS.push({currVal, 1});
-    while((currR != 1 || currC != 1) && !abS.empty())
-    {
-        AB topAB = abS.top();
-        changeGlassDir(currGlassDir, currR, currC);
-        currR += glassDir[currGlassDir][0]; currC += glassDir[currGlassDir][1];
-        currVal = G[currR][currC];
-        if(currVal == 0)
-        {
-            break;
-        }
-        if(topAB.A == currVal)
-        {
-            abS.pop();
-            topAB.B += 1;
-            abS.push(topAB);
-        }
-        else
-        {
-            abS.push({currVal, 1});
-        }
-    }
-    if(abS.size() == 1)
-    {
-        queue<int> savedQ;
-        initG(savedQ);
-        return;
-    }
-    stack<int> savedS;
-    while(!abS.empty())
-    {
-        AB t = abS.top(); abS.pop();
-        savedS.push(t.A);
-        savedS.push(t.B);
-    }
-
-    queue<int> savedQ;
-    while(!savedS.empty())
-    {
-        int t = savedS.top(); savedS.pop();
-        savedQ.push(t);
-    }
-
-    initG(savedQ);
-}
-
-void printExplodedGalss()
-{
-    cout << aEx + bEx*2 + cEx*3;
-}
-
-void printG()
-{
-    cout << "\n====print GGG=====\n";
-    for(int r=1; r<=N; ++r)
-    {
-        for(int c=1; c<=N; ++c)
-        {
+void printG(){
+    cout << "\n=======\n";
+    for(int r=1; r<=N; ++r){
+        for(int c=1; c<=N; ++c){
             cout << G[r][c] << " ";
+        }cout << "\n";
+    }
+}
+
+void removeTails(){
+    vector<int> v;
+    for(int i=N*N-1; i>=0; --i){
+        pos curr = m[i];
+        if(G[curr.r][curr.c] != 0 && v.size() == 0){
+            v.push_back(i);
+            continue;
         }
-        cout << "\n";
+        if(G[curr.r][curr.c] != 0){
+            v.push_back(i);
+            continue;
+        }
+        if(i == 0){
+            for(auto vv: v){
+                v.pop_back();
+            }
+        }
+        if(G[curr.r][curr.c] == 0 && v.size() > 0){
+            break;
+        }
+    }
+    for(auto vv: v){
+        pos curr = m[vv];
+        G[curr.r][curr.c] = 0;
     }
 }
 
-void solve()
-{
-    getInput();
-    for(int i=0; i<M; ++i)
-    {   if(N == 33 && M == 79){cout << 442; return;}
-        shootBlizard();
-        moveGlass();
-        explodeGlass();
-        resetG();
+void moveBalls(){
+    int posDiff = 0;
+    for(int i=1; i<N*N; ++i){
+        pos curr = m[i];
+        if(posDiff+i >= N*N) break;
+        pos next = m[i+posDiff];
+        while(G[next.r][next.c] == 0 && !outOfBound(next.r, next.c)) {
+            ++posDiff;
+            next = m[i+posDiff];
+        }
+        G[curr.r][curr.c] = G[next.r][next.c];
     }
-    printExplodedGalss();
+    while(posDiff--){
+        pos curr = m[N*N-1-posDiff];
+        G[curr.r][curr.c] = 0;
+    }
+    removeTails();
 }
 
-int main()
-{
+void countBoomGroup(int ballNum, int boomCnt){
+    if(ballNum == 1){
+        g1 += boomCnt;
+    }
+    if(ballNum == 2){
+        g2 += boomCnt;
+    }
+    if(ballNum == 3){
+        g3 += boomCnt;
+    }
+}
+
+bool boomBalls(){
+    int ballNum = G[m[1].r][m[1].c];
+    int idx = 1;
+    int boomCnt = 1;
+    bool flag = false;
+    for(int i=2; i<N*N; ++i){
+        pos curr = m[i];
+        int currBallNum = G[curr.r][curr.c];
+        if(ballNum == currBallNum){
+            ++boomCnt;
+            continue;
+        }
+
+        ballNum = currBallNum;
+        if(currBallNum == 0){
+            if(boomCnt >= 4){
+                int tempI = i-1;
+                flag = true;
+                ballNum = G[m[tempI].r][m[tempI].c];
+                countBoomGroup(ballNum, boomCnt);
+                while(boomCnt--){
+                    pos now = m[tempI--];
+                    G[now.r][now.c] = 0;
+                }
+            } else{
+                boomCnt = 0;
+            }
+            break;
+        }
+        if(boomCnt >= 4){
+            int tempI = i-1;
+            flag = true;
+            int removeBallNum = G[m[tempI].r][m[tempI].c];
+            countBoomGroup(removeBallNum, boomCnt);
+            while(boomCnt--){
+                pos now = m[tempI--];
+                G[now.r][now.c] = 0;
+            }
+        }
+        boomCnt = 1;
+    }
+    
+    if(boomCnt >= 4){
+        int tempI = N*N-1;
+        countBoomGroup(ballNum, boomCnt);
+        while(boomCnt--){
+            pos now = m[tempI--];
+            G[now.r][now.c] = 0;
+        }
+    }
+
+    return flag;
+}
+
+void changeBalls(){
+    queue<group> q;
+    int ballNum = G[m[1].r][m[1].c];
+    int sameBallCnt = 1;
+    for(int i=2; i<N*N; ++i){
+        pos curr = m[i];
+        int currBallNum = G[curr.r][curr.c];
+        if(currBallNum == 0){
+            q.push({ballNum, sameBallCnt});
+            sameBallCnt = 0;
+            break;
+        }
+        if(ballNum == currBallNum){
+            ++sameBallCnt;
+            continue;
+        }
+        q.push({ballNum, sameBallCnt});
+        ballNum = currBallNum;
+        sameBallCnt = 1;
+    }
+    if(sameBallCnt >= 1){
+        q.push({ballNum, sameBallCnt});
+    }
+
+    int idx = 1;
+    while(!q.empty()){
+        group currGroup = q.front(); q.pop();
+        if(idx > N*N) break;
+        pos cntPos = m[idx++];
+        pos groupPos = m[idx++];
+        G[cntPos.r][cntPos.c] = currGroup.cnt;
+        G[groupPos.r][groupPos.c] = currGroup.g;
+    }
+}
+
+void solve(){
+    getInputs();
+    setChangeDirPos();
+    setGIndex();
+    for(int i=0; i<M; ++i){
+        blizard(i);
+        // cout << "\nafter blizard\n"; printG();
+        moveBalls();
+        // cout << "\nafter move balls\n"; printG();
+        while(boomBalls()){
+            // cout << "\nafter boom balls\n"; printG();
+            moveBalls();
+            // cout << "\nafter move balls\n"; printG();
+        };
+        changeBalls();
+        // cout << "\nafter change balls\n"; printG();
+    }
+    cout << g1+2*g2+3*g3 << "\n";
+}
+
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     solve();
 
     return 0;
