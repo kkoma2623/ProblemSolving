3aaa020024b34a7bac366df597f9386b25f2d971
kkoma2623 <jiwook.dev@gmail.com>
2022-10-04 19:46:27 +0900

    add template

---
+++ Baekjoon/0_template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void getInput(){
+
+}
+
+void solve(){
+  getInput();
+  
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
