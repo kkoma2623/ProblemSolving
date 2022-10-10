2df96f66cae42a377d5f103ea4fce453f1b3d064
kkoma2623 <jiwook.dev@gmail.com>
2022-10-10 22:02:11 +0900

    1967 Tree

---
+++ Baekjoon/01967Baek.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define MAX_N 10000
+
+using namespace std;
+
+int N;
+struct edge{
+  int v, w;
+
+  bool operator<(const edge &cmp)const{
+    return w < cmp.w;
+  }
+  bool operator>(const edge &cmp)const{
+    return w > cmp.w;
+  }
+};
+vector<edge> G[MAX_N+1];
+bool visited[MAX_N+1];
+
+void getInputs(){
+  cin >> N;
+  for(int i=0; i<N-1; ++i){
+    int p, c, w;
+    cin >> p >> c >> w;
+    G[p].push_back({c, w});
+    G[c].push_back({p, w});
+  }
+}
+
+pair<int, int> findEndV(int currV){
+  int retV = currV;
+  int maxi = 0;
+  priority_queue<edge, vector<edge>, greater<edge>> pq;
+  pq.push({currV, 0});
+  visited[currV] = true;
+
+  while(!pq.empty()){
+    edge curr = pq.top(); pq.pop();
+    if(curr.w > maxi){
+      maxi = curr.w;
+      retV = curr.v;
+    }
+    for(auto next: G[curr.v]){
+      if(visited[next.v]) continue;
+      pq.push({next.v, next.w + curr.w});
+      visited[next.v] = true;
+    }
+  }
+
+  return {retV, maxi};
+}
+
+void solve(){
+  getInputs();
+  pair<int, int> endV = findEndV(1);
+  for(int i=1; i<=MAX_N; ++i){
+    visited[i] = false;
+  }
+  pair<int, int> end2EndV = findEndV(endV.first);
+  cout << end2EndV.second << "\n";
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
