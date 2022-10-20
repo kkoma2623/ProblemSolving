f4642b9b38f148a5b54f35bf87960c98ea6b1207
kkoma2623 <jiwook.dev@gmail.com>
2022-10-20 20:07:08 +0900

    11726 dp

---
+++ Baekjoon/11726Baek.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#define MAX_N 1000
+
+using namespace std;
+
+unsigned long long N;
+
+unsigned long long dp[MAX_N+1];
+
+void getInputs(){
+  cin >> N;
+  dp[0] = 1;
+  dp[1] = 1;
+  dp[2] = 2;
+}
+
+void solve(){
+  getInputs();
+  for(int i=3; i<=N; ++i){
+    dp[i] = (dp[i-1] + dp[i-2])%10007;
+  }
+  cout << dp[N] << "\n";
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
